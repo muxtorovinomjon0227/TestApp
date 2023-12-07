bdbf04efb6c90c60de2bbc34f5e545de5e24792d
Muktorov <muxtorovinomjon0227@gmail.com>
2023-12-07 20:43:05 +0500

    First commit for TestApp

---
+++ lib/src/constants/api_constants/api_const.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
+class BaseApi {
+  static const String SERVER_HOST = 'https://cld.alovoice.uz/mapi'; // TODO: This server hosting url => Serverni host qismi
+
+  static const String SIGN_IN = "$SERVER_HOST/mlogin"; // TODO: This api POST type  Bu api userni login qilish uchun
+
+}
---
+++ lib/src/constants/colors_const.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorConst {
+  static Color whiteColor = const Color(0xffFFFFFF);
+  static Color bleackColor = const Color(0xff000000);
+  static Color appBackgroundColor = const Color(0xffF3F4F6);
+
+
+}
